5da788701bffca347b8fd2998ad6c3d0aa13b20d
free99 <ja_bouchouareb@esi.dz>
2021-05-15 02:20:39 +0100

    added stars to home screen need to fix respo

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:astro01/components/constants.dart';
+import 'splashScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -17,58 +18,88 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Stack(
+            alignment: Alignment.center,
             children: [
               Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  AutoSizeText(
-                    'Salut Anis !',
-                    style: TextStyle(
-                      color: myRed,
-                      fontWeight: FontWeight.w900,
-                      fontSize: 45,
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AutoSizeText(
+                        'Salut Anis !',
+                        style: TextStyle(
+                          color: myRed,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 45,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      AutoSizeText(
+                        'Zinou nhar el lyoum mabrok idkom',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w100,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  AutoSizeText(
-                    'Zinou nhar el lyoum mabrok idkom',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w100,
-                      fontSize: 15,
-                    ),
+                  Column(
+                    children: [
+                      SelectBox(
+                        image: 'astroReading',
+                        text: 'Decouvrire',
+                        color: myRed,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      SelectBox(
+                        image: 'ridingRocket',
+                        text: 'Jouer',
+                        color: Color(0xffAB02E6),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      SelectBox(
+                        image: 'ridingMoon',
+                        text: 'Profile',
+                        color: Color(0xff1759BC),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              Column(
-                children: [
-                  SelectBox(
-                    image: 'astroReading',
-                    text: 'Decouvrire',
-                    color: myRed,
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  SelectBox(
-                    image: 'ridingRocket',
-                    text: 'Jouer',
-                    color: Color(0xffAB02E6),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  SelectBox(
-                    image: 'ridingMoon',
-                    text: 'Profile',
-                    color: Color(0xff1759BC),
-                  ),
-                ],
+              Positioned(
+                top: 20,
+                left: 235,
+                child: Star(angle: 8, scale: 2.8),
+              ),
+              Positioned(
+                top: 150,
+                left: 330,
+                child: Star(angle: 5, scale: 2.8),
+              ),
+              Positioned(
+                top: 550,
+                left: 0,
+                child: Star(angle: 8, scale: 2.8),
+              ),
+              Positioned(
+                top: 750,
+                left: 330,
+                child: Star(angle: 8, scale: 2.8),
+              ),
+              Positioned(
+                top: 780,
+                left: 40,
+                child: Star(angle: 8, scale: 2.8),
               ),
             ],
           ),

8bc43bb37258b2a9cf821274018dbd414a54d050
NadirOuagued <58882164+nadir-ogd@users.noreply.github.com>
2021-05-29 14:40:10 +0100

    conflicts fixed

--- lib/Screens/quiz.dart
+++ lib/Screens/quiz.dart
@@ -24,12 +24,8 @@ import 'package:provider/provider.dart';
 //   return rootBundle.loadString('questions.json');
 // }
 List<String> propo = ['a', 'b', 'c', 'd'];
-<<<<<<< HEAD
 int points = 0;
 bool cliquer = false;
-=======
-
->>>>>>> d815ee1542266a4ac14070a8130c16b0b9d64648
 List<int> ind = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 class Ind extends ChangeNotifier {
@@ -178,7 +174,6 @@ class _AnswerBoxState extends State<AnswerBox> {
                 onTap: () {
                   if (widget.answer == propo[0]) {
                     setState(() {
-<<<<<<< HEAD
                       if (cliquer == false) {
                         points++;
                       }
@@ -191,16 +186,6 @@ class _AnswerBoxState extends State<AnswerBox> {
                         if (verification(points) == 1) {
                           update();
                         }
-=======
-                      choiceColor = choiceColors[0];
-                      questNum++;
-                      ind.removeAt(0);
-                      // if ((ind.isEmpty) && (questNum == 10)) {
-                      if (ind.isEmpty) {
-                        print('ok');
-                        Navigator.pushNamed(context, '/planetChoice');
-                        questNum = 1;
->>>>>>> d815ee1542266a4ac14070a8130c16b0b9d64648
                       } else {
                         //var route = new MaterialPageRoute(
                         //builder: (BuildContext context) => new Quiz(
@@ -211,10 +196,7 @@ class _AnswerBoxState extends State<AnswerBox> {
                     });
                   } else if (widget.answer == propo[1])
                     setState(() {
-<<<<<<< HEAD
                       cliquer = true;
-=======
->>>>>>> d815ee1542266a4ac14070a8130c16b0b9d64648
                       choiceColor = choiceColors[1];
                     });
                   else if (widget.answer == propo[2])
@@ -223,10 +205,7 @@ class _AnswerBoxState extends State<AnswerBox> {
                     });
                   else if (widget.answer == propo[3])
                     setState(() {
-<<<<<<< HEAD
                       cliquer = true;
-=======
->>>>>>> d815ee1542266a4ac14070a8130c16b0b9d64648
                       choiceColor = choiceColors[3];
                     });
 
@@ -241,24 +220,17 @@ class _AnswerBoxState extends State<AnswerBox> {
                       setState(() {
                         choiceColor = Colors.white;
                       });
-<<<<<<< HEAD
-
-=======
->>>>>>> d815ee1542266a4ac14070a8130c16b0b9d64648
                       if (ind.isNotEmpty)
                         setState(() {
                           Provider.of<Ind>(context, listen: false)
                               .updateAvatar(ind);
                         });
-<<<<<<< HEAD
                       else {
                         Navigator.pushReplacementNamed(
                             context, '/planetChoice');
                         questNum = 1;
                         points = 0;
                       }
-=======
->>>>>>> d815ee1542266a4ac14070a8130c16b0b9d64648
                     }
                   });
                 },
@@ -396,7 +368,6 @@ class AppbarCustomed extends StatelessWidget {
                     ),
                     Spacer(flex: 5),
                     IconButton(
-<<<<<<< HEAD
                         icon: Icon(Icons.clear),
                         color: myRed2,
                         iconSize: 20,
@@ -404,14 +375,6 @@ class AppbarCustomed extends StatelessWidget {
                           points = 0;
                           Navigator.pushNamed(context, '/planetChoice');
                         }),
-=======
-                      icon: Icon(Icons.clear),
-                      color: myRed2,
-                      iconSize: 20,
-                      onPressed: () =>
-                          Navigator.pushNamed(context, '/planetChoice'),
-                    ),
->>>>>>> d815ee1542266a4ac14070a8130c16b0b9d64648
                   ],
                 ),
               ]),
@@ -494,7 +457,6 @@ List shuffle(List<int> indices) {
 
   return indices;
 }
-<<<<<<< HEAD
 
 int verification(int point) {
   if (planeteInd == 0) {
@@ -596,5 +558,3 @@ void update() async {
       .eq("email", user.email)
       .execute();
 }
-=======
->>>>>>> d815ee1542266a4ac14070a8130c16b0b9d64648

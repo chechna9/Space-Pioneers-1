481cb2f4f5f36a113a3fea2762bdd124af43ec6e
free99 <ja_bouchouareb@esi.dz>
2021-06-10 13:28:51 +0100

    added Original splashScreen & music under fixing

--- lib/Screens/OriginalSplashScreen.dart
+++ lib/Screens/OriginalSplashScreen.dart
@@ -1,3 +1,5 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:astro01/components/constants.dart';
 import 'package:astro01/variable_globale/variable.dart';
 import 'package:flutter/material.dart';
 import 'package:injector/injector.dart';
@@ -10,21 +12,41 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
-  @override
-  void initState() {
-    super.initState();
-    final Supabaseclient = Injector.appInstance.get<SupabaseClient>();
-    final userr = supabaseclient.auth.user();
-    if (userr == null) {
-      Navigator.pushNamed(context, '/splashScreen');
-    } else {
-      user.email = userr.email;
-      Navigator.pushNamed(context, '/homeScreen');
-    }
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   final Supabaseclient = Injector.appInstance.get<SupabaseClient>();
+  //   final userr = supabaseclient.auth.user();
+  //   if (userr == null) {
+  //     Navigator.pushNamed(context, '/splashScreen');
+  //   } else {
+  //     user.email = userr.email;
+  //     Navigator.pushNamed(context, '/homeScreen');
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      child: Container(
+        color: myRed,
+        child: Center(
+          child: DefaultTextStyle(
+            style: const TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900,
+            ),
+            child: AnimatedTextKit(
+              animatedTexts: [
+                WavyAnimatedText('SPACE PIONEERS'),
+              ],
+              // isRepeatingAnimation: true,
+              displayFullTextOnTap: false,
+              repeatForever: true,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
